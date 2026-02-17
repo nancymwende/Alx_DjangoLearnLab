57820f050057f6ac0fe42077130453af05eb146f
nancymwende <nancymwende860@gmail.com>
2026-02-17 10:25:54 +0300

    modify views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
+from django.shortcuts import render # type: ignore
+from django.views.generic.detail import DetailView # type: ignore
 from .models import Book
 from .models import Library
 
