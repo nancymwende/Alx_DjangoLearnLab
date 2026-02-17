d3c491363b98ef95f5a382a9e2efb2826d75669d
nancymwende <nancymwende860@gmail.com>
2026-02-17 10:33:55 +0300

    modify urls.py & add .gitignore file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,12 +3,10 @@ from django.views.generic.detail import DetailView # type: ignore
 from .models import Book
 from .models import Library
 
-# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view to display details for a specific library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
