f1823eed0c2d60113889faa20626a123c2a7ca5d
nancymwende <nancymwende860@gmail.com>
2026-02-17 09:42:45 +0300

    modify urls.py, rdelete and recreate html files in relationship_app

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -17,11 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from relationship_app.views import list_books
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', list_books, name='home'),  # Homepage now shows books
     path('', include('relationship_app.urls')),
 ]
 
