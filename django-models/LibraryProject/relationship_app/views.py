222696eec02a7b29faf8e23e23c5333101390bc2
nancymwende <nancymwende860@gmail.com>
2026-02-17 11:20:54 +0300

    create new html files & modify urls.py files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render # type: ignore
 from django.views.generic.detail import DetailView # type: ignore
 from .models import Book
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 def list_books(request):
     books = Book.objects.all()
@@ -12,3 +14,14 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+# User Registration View
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'relationship_app/register.html', {'form': form})
