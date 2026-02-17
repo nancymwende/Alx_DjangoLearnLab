a07b55a451b0d58ce2c80b7817da32c1a35d4b21
nancymwende <nancymwende860@gmail.com>
2026-02-17 12:25:04 +0300

    create html files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from .models import Book
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm # type: ignore
 from django.shortcuts import redirect # type: ignore
+from django.contrib.auth.decorators import user_passes_test
 
 
 
@@ -26,3 +27,19 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+
+# Admin-only view
+@user_passes_test(lambda u: u.is_authenticated and u.userprofile.role == 'Admin')
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian-only view
+@user_passes_test(lambda u: u.is_authenticated and u.userprofile.role == 'Librarian')
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member-only view
+@user_passes_test(lambda u: u.is_authenticated and u.userprofile.role == 'Member')
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
