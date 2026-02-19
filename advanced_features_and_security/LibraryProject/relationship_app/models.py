2ff5a7c00217f92b623d58a28012904e902916b5
nancymwende <nancymwende860@gmail.com>
2026-02-19 11:02:32 +0300

    create a superuser and modify models.py

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -55,14 +56,62 @@ class UserProfile(models.Model):
         ('Librarian', 'Librarian'),
         ('Member', 'Member'),
     ]
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
 # Automatically create UserProfile when a new User is created
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, date_of_birth=None, profile_photo=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username field must be set")
+
+        email = self.normalize_email(email)
+        user = self.model(
+            username=username,
+            email=email,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, date_of_birth=None, profile_photo=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(
+            username,
+            email=email,
+            password=password,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo,
+            **extra_fields
+        )
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+ 
