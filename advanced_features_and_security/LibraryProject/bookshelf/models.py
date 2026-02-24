13b564cb561f62bf948209c7f9d1b06ee7e489e6
nancymwende <nancymwende860@gmail.com>
2026-02-24 11:23:59 +0300

    remove and create new bookshelf migrations

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -4,8 +4,15 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    author = models.CharField(max_length=200)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
 
     def __str__(self):
         return self.title
