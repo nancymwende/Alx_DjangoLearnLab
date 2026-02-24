0fffba0ebfcd4a801861a9881a11addc8c2a1066
nancymwende <nancymwende860@gmail.com>
2026-02-24 14:25:50 +0300

    create file in bookshelf

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('books/', include('relationship_app.urls')),
+    path('', include('bookshelf.urls')),
     path('', RedirectView.as_view(pattern_name='list_books', permanent=False)),
 ]
 
