57d60788e3fe4fe320ce9f9ed14cc89e00d9deed
nancymwende <nancymwende860@gmail.com>
2026-02-25 10:08:07 +0300

    configure settings.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from django.db.models import Q
+from .forms import BookForm
 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
+
+def search_books(request):
+    query = request.GET.get('q')
+
+    if query:
+        books = Book.objects.filter(title__icontains=query)
+    else:
+        books = Book.objects.all()
+
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
+def add_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = BookForm()
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
