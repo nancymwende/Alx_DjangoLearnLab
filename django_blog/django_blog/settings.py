44fa3a553979fbf9b0a791addfd324a9abadcbb6
nancymwende <nancymwende860@gmail.com>
2026-03-08 20:37:37 +0300

    modify settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,9 +78,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
