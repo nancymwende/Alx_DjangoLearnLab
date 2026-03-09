339c2ac709bfa48eca70012fe5eb526ddf332f28
nancymwende <nancymwende860@gmail.com>
2026-03-09 10:12:56 +0300

    aunthenticate user

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,6 +6,7 @@ from .views import (
     PostUpdateView,
     PostDeleteView,
 )
+from blog import views
 
 urlpatterns = [
     path("", PostListView.as_view(), name="home"),
@@ -14,4 +15,7 @@ urlpatterns = [
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("post/<int:pk>/comments/new/", views.add_comment, name="add-comment"),
+    path("comment/<int:pk>/edit/", views.edit_comment, name="edit-comment"),
+    path("comment/<int:pk>/delete/", views.delete_comment, name="delete-comment"),
 ]
