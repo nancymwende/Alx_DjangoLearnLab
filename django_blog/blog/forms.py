0afe4c97633dc60ff2340769f9e3588f00087b05
nancymwende <nancymwende860@gmail.com>
2026-03-09 09:37:36 +0300

    create comment form

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post
+from .models import Comment
 
 
 class RegisterForm(UserCreationForm):
@@ -16,3 +17,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
