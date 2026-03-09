ffc451d98babbc51ced92ed2535f98a14bfd7d47
nancymwende <nancymwende860@gmail.com>
2026-03-09 11:01:43 +0300

    create and modify tag model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -25,3 +25,22 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
+
+    def __str__(self):
+        return self.title
