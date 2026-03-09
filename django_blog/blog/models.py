bdcf343aef6439b79861e7a88b90f88f4c6e19f9
nancymwende <nancymwende860@gmail.com>
2026-03-09 13:12:57 +0300

    modify views

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class Post(models.Model):
@@ -8,6 +9,8 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_posted = models.DateTimeField(auto_now_add=True)
+    # Use TaggableManager for tags
+    tags = TaggableManager(blank=True)
 
     def __str__(self):
         return self.title
@@ -25,31 +28,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
-
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
-
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
-
-    def __str__(self):
-        return self.title
-
-
-from taggit.managers import TaggableManager
-
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-
-    tags = TaggableManager()
