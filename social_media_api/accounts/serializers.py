816893b5bc20ba438c7e1961f3dd1db99461859e
nancymwende <nancymwende860@gmail.com>
2026-03-11 13:20:05 +0300

    modify models

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -14,19 +14,13 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password', 'bio']
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', '')
+        )
+
         Token.objects.create(user=user)
+
         return user
-
-
-class LoginSerializer(serializers.Serializer):
-
-    username = serializers.CharField()
-    password = serializers.CharField()
-
-
-class UserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
