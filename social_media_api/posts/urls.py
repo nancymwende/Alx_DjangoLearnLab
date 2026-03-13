c6d47950af83cc0f1f548df2c2eb44eab9b5507d
nancymwende <nancymwende860@gmail.com>
2026-03-13 10:44:40 +0300

    modify urls

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,8 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import PostViewSet, CommentViewSet
+from .views import feed
+
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
@@ -8,4 +10,5 @@ router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('feed/', feed, name='feed'), 
 ]
